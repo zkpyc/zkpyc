71ed792cdbb2308430465abe59ef382e7eed05d6
Lorenzo Rota <lorenzo.rota@tno.nl>
2023-12-22 08:16:50 +0100

    Removed MPyC runtime instantiation for initializing finite fields

--- examples/zk_types/types.py
+++ examples/zk_types/types.py
@@ -1,17 +1,18 @@
-from typing import NewType, TypeVar, Generic
-import mpyc
-from mpyc.runtime import mpc
+from typing import NewType, TypeVar, Generic, Any
+from mpyc import finfields
 
-T = TypeVar('T')
+T = TypeVar('T', bound=Any)
 N = TypeVar('N')
 
 # field = NewType('field', int)
 
 bn256_scalar_modulus = 21888242871839275222246405745257275088548364400416034343698204186575808495617
 bls12_381_scalar_modulus = 52435875175126190479447740508185965837690552500527637822603658699938581184513
+curve25519_scalar_field_modulus = 7237005577332262213973186563042994240857116359379907606001950938285454250989
 
-# field = mpyc.finfields.GF(bn256_scalar_modulus)
-field = mpyc.finfields.GF(bls12_381_scalar_modulus)
+# field = finfields.GF(bn256_scalar_modulus)
+field = finfields.GF(bls12_381_scalar_modulus)
+# field = finfields.GF(curve25519_scalar_field_modulus)
 
 class Public(Generic[T]):
     pass
@@ -22,7 +23,3 @@ class Private(Generic[T]):
 class Array(Generic[T, N]):
     def __getitem__(self, key: int) -> T:
         return self[key]
-
-# Public = NewType('Public', Array)
-
-# Private = NewType('Private', Array)
--- zkpyc_stdlib/stdlib/zk_types/types.py
+++ zkpyc_stdlib/stdlib/zk_types/types.py
@@ -1,6 +1,5 @@
 from typing import NewType, TypeVar, Generic, Any
-import mpyc
-from mpyc.runtime import mpc
+from mpyc import finfields
 
 T = TypeVar('T', bound=Any)
 N = TypeVar('N')
@@ -9,9 +8,11 @@ N = TypeVar('N')
 
 bn256_scalar_modulus = 21888242871839275222246405745257275088548364400416034343698204186575808495617
 bls12_381_scalar_modulus = 52435875175126190479447740508185965837690552500527637822603658699938581184513
+curve25519_scalar_field_modulus = 7237005577332262213973186563042994240857116359379907606001950938285454250989
 
-# field = mpyc.finfields.GF(bn256_scalar_modulus)
-field = mpyc.finfields.GF(bls12_381_scalar_modulus)
+# field = finfields.GF(bn256_scalar_modulus)
+field = finfields.GF(bls12_381_scalar_modulus)
+# field = finfields.GF(curve25519_scalar_field_modulus)
 
 class Public(Generic[T]):
     pass
@@ -22,7 +23,3 @@ class Private(Generic[T]):
 class Array(Generic[T, N]):
     def __getitem__(self, key: int) -> T:
         return self[key]
-
-# Public = NewType('Public', Array)
-
-# Private = NewType('Private', Array)
